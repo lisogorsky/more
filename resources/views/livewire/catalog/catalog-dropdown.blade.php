2aafb85517db476fcfbedc6969ea34f31eac2e4b
Dima <olimpkemerovo@gmail.com>
2026-01-26 12:02:12 +0700

    feat:изменил каталог

--- resources/views/livewire/catalog/catalog-dropdown.blade.php
+++ resources/views/livewire/catalog/catalog-dropdown.blade.php
@@ -1,67 +1,56 @@
-<div class="position-relative" x-data="{ open: false, activeCategory: null }">
-    <!-- Кнопка Каталог -->
-    <button @click="open = !open" class="btn btn-primary text-dark">
-        Каталог
-    </button>
+<div>
+    <div x-data="{ open: false }" class="position-relative">
+        <button @click="open = !open" class="btn btn-primary d-flex align-items-center gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid"
+                viewBox="0 0 16 16">
+                <path
+                    d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5z" />
+            </svg>
+            Каталог
+        </button>
 
-    <!-- Mega Menu -->
-    <div x-show="open" x-transition @click.away="open = false" class="position-absolute bg-white shadow overflow-auto"
-        style="top: 50px; width: 90vw; height: 50vh; background-color: rgba(255, 255, 255, 0.95); z-index: 1050; padding: 20px; color: black;">
-        <div class="row h-100">
-            <!-- Слева: Основные категории -->
-            <div class="col-4 border-end h-100 overflow-auto">
-                <ul class="list-unstyled mb-0">
-                    @foreach ($categories as $category)
-                        <li class="mb-2">
-                            <div class="p-2 rounded" :class="{ 'bg-light': activeCategory === {{ $category->id }} }"
-                                style="cursor: pointer;" {{-- hover: только визуал + подгрузка подкатегорий --}}
-                                @mouseenter="
-        activeCategory = {{ $category->id }};
-        $wire.set('selectedCategory', {{ $category->id }});
-        $wire.set('selectedSubcategory', null);
-    "
-                                {{-- click: выбор категории + фильтр товаров --}}
-                                @click="
-        $wire.toggleCategory({{ $category->id }});
-        Livewire.dispatch('categorySelected', { categoryId: {{ $category->id }} });
-        open = false;
-        ">
-                                @if ($category->icon_svg)
-                                    <span class="me-2">{!! $category->icon_svg !!}</span>
-                                @endif
-                                {{ $category->name }}
-                            </div>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
+        <div x-show="open" x-transition:enter="transition ease-out duration-200"
+            x-transition:enter-start="opacity-0 transform scale-95"
+            x-transition:enter-end="opacity-100 transform scale-100" @click.away="open = false"
+            class="position-absolute start-0 bg-white shadow-lg border rounded-3 p-4"
+            style="z-index: 9999; width: 95vw; max-width: 1200px; top: 110%; left: 0;">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 overflow-auto"
+                style="max-height: 70vh;">
+                @foreach ($categories as $category)
+                    <div class="col">
+                        <h6 class="fw-bold text-uppercase mb-2 pb-1 border-bottom d-flex align-items-center"
+                            style="color: #333; cursor: pointer;"
+                            @click="$wire.toggleCategory({{ $category->id }}); open = false;">
+                            @if ($category->icon_svg)
+                                <span class="me-2 d-inline-block" style="width: 20px;">{!! $category->icon_svg !!}</span>
+                            @endif
+                            {{ $category->name }}
+                        </h6>
 
-            <!-- Справа: Подкатегории выбранной категории -->
-            <div class="col-8 h-100 overflow-auto" x-data="{ activeSubcategory: null }">
-                <ul class="list-unstyled mb-0">
-                    @if ($categories)
-                        @php
-                            $selected = $categories->firstWhere('id', $selectedCategory);
-                        @endphp
-                        @if ($selected && $selected->subcategories)
-                            @foreach ($selected->subcategories as $sub)
-                                <li class="p-2 rounded" style="cursor: pointer;"
-                                    :class="{ 'bg-light': activeSubcategory === {{ $sub->id }} }"
-                                    {{-- hover: визуал --}} @mouseover="activeSubcategory = {{ $sub->id }}"
-                                    @mouseout="activeSubcategory = null" {{-- click: бизнес-логика --}}
-                                    @click="
-        $wire.selectSubcategory({{ $sub->id }});
-        Livewire.dispatch('subcategorySelected', { subcategoryId: {{ $sub->id }} });
-    open = false">
-                                    {{ $sub->name }}
+                        <ul class="list-unstyled">
+                            @foreach ($category->subcategories as $sub)
+                                <li class="mb-1">
+                                    <a href="#" class="text-decoration-none text-secondary small hover-blue"
+                                        @click.prevent="
+                                        $wire.selectSubcategory({{ $sub->id }});
+                                        Livewire.dispatch('subcategorySelected', { subcategoryId: {{ $sub->id }} });
+                                        open = false;
+                                   ">
+                                        {{ $sub->name }}
+                                    </a>
                                 </li>
                             @endforeach
-                        @else
-                            <li class="p-2 text-muted">Выберите категорию слева</li>
-                        @endif
-                    @endif
-                </ul>
+                        </ul>
+                    </div>
+                @endforeach
             </div>
         </div>
+
+        <style>
+            .hover-blue:hover {
+                color: #0d6efd !important;
+                text-decoration: underline !important;
+            }
+        </style>
     </div>
 </div>
